2ac2cfc2318c1dc66e5be251c9e7e3a5161ecdc2
ArtemOliynyk21 <temaneo700@gmail.com>
2022-08-20 11:11:15 +0300

    task-1-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const markupGallery = document.querySelector(".gallery");
+
+const imageGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join(" ");
+console.log(imageGallery);
+
+markupGallery.insertAdjacentHTML("beforeend", `${imageGallery}`);
+
+const backdroup = basicLightbox.create(`<img src="">`, {
+  onShow: (backdroup) => {
+    window.addEventListener("keydown", escClick);
+  },
+  onClose: (backdroup) => {
+    window.removeEventListener("keydown", escClick);
+  },
+});
+
+const clickImage = (evt) => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  backdroup.element().querySelector("img").src = evt.target.dataset.source;
+  backdroup.show();
+};
+
+markupGallery.addEventListener("click", clickImage);
+
+const escClick = (evt) => {
+  if (evt.key === "Escape") {
+    backdroup.close();
+    return;
+  }
+};
+
 console.log(galleryItems);
